7d19515bee1b80a1a9fff92c1a870f68363a1385
RockerBot <rockerbotmail@gmail.com>
2023-04-27 16:52:50 +0530

    hotfix: disableFrmToolbar works with the skipping

--- skipAlong/skipAlong.js
+++ skipAlong/skipAlong.js
@@ -1,15 +1,15 @@
 const observer = new MutationObserver(function(mutations) {
-    var enabled;
-    browser.storage.local.get(['skipAlong_enabled'], (result) => { enabled = result.variable; });
-    if(!enabled)return;
-    for (let mutation of mutations) {
-        if ( (typeof(mutation.target.className) == "string") && mutation.target.className.includes("ad") ){
-            const elements = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
-            if (elements.length !==0) {
-                elements[0].click();
-                break;
+    browser.storage.local.get(['skipAlong_enabled'], (result) => {
+        if(!result.skipAlong_enabled)return;
+        for (let mutation of mutations) {
+            if ( (typeof(mutation.target.className) == "string") && mutation.target.className.includes("ad") ){
+                const elements = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
+                if (elements.length !==0) {
+                    elements[0].click();
+                    break;
+                }
             }
         }
-    }
+    });
 });  
 observer.observe(document.body, { childList: true, subtree: true, attributes: true });
