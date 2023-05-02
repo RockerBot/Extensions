eceae766c47ca7419fc0525c756fc04280f61310
RockerBot <rockerbotmail@gmail.com>
2023-05-02 22:23:34 +0530

    helpfulX10sion, removesShortsBtn

---
+++ tidBits/utube.js
@@ -0,0 +1,30 @@
+
+aabb = {}
+const observer = new MutationObserver(function(mutations) {
+    el = document.querySelector('[title="Shorts"]')
+    if(el !==null)el.remove();
+    browser.storage.local.get(['tidBits_enabled'], (result) => {
+        if(!result.tidBits_enabled)return;
+        for (let elem of document.getElementsByTagName("ytd-reel-shelf-renderer")) {
+            elem.remove();
+        }
+    });
+});  
+observer.observe(document.body, { childList: true, subtree: true, attributes: true });
+
+
+
+// for (elem of document.getElementsByTagName("video")){
+//     aabb[elem.src] = 1;
+// }
+// console.log(aabb)
+// obj ={ "": 1,
+//  "blob:https://www.youtube.com/214374c1-d5bc-4577-bb21-939c7b91db10": 1,
+//  "blob:https://www.youtube.com/21efee93-af26-49b3-b3ed-02410d13ffab": 1,
+//  "blob:https://www.youtube.com/f937b475-a37f-4471-9838-0bc34c4d4553": 1,
+//  "blob:https://www.youtube.com/e56a72a8-0b20-45e6-909b-7c3002f40e4f": 1,
+//  "blob:https://www.youtube.com/7108baa1-22c5-42c5-82b0-ce4b0f481ca3": 1,
+//  "blob:https://www.youtube.com/5bcd0b3d-f32e-496f-9fe0-9679b45a427a": 1,
+//  "blob:https://www.youtube.com/af3b1285-1a47-4390-a94b-42e604d2642b": 1,
+//  "blob:https://www.youtube.com/c8f944a2-8bd5-4e76-bf2f-e60a7c7da51d": 1
+// };
