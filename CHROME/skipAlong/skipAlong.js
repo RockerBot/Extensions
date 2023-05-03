3ba1087363f29d4d9c1268db6854c825e38159aa
RockerBot <rockerbotmail@gmail.com>
2023-05-03 14:22:08 +0530

    chrome porting update

--- CHROME/skipAlong/skipAlong.js
+++ CHROME/skipAlong/skipAlong.js
@@ -1,6 +1,9 @@
 const observer = new MutationObserver(function(mutations) {
     chrome.storage.local.get(['skipAlong_enabled'], (result) => {
         if(!result.skipAlong_enabled)return;
+        handleID("player-ads")
+        handleID("masthead-ad")
+        handleTagName("ytd-ad-slot-renderer")
         for (let mutation of mutations) {
             if ( (typeof(mutation.target.className) == "string") && mutation.target.className.includes("ad") ){
                 const elements = document.getElementsByClassName("ytp-ad-skip-button ytp-button");
@@ -13,3 +16,10 @@ const observer = new MutationObserver(function(mutations) {
     });
 });  
 observer.observe(document.body, { childList: true, subtree: true, attributes: true });
+function handleID(id){
+    elm = document.getElementById(id)
+    if(elm !== null) elm.remove();
+}
+function handleTagName(tagname){
+    for(elem of document.getElementsByTagName(tagname))elem.remove();
+}
