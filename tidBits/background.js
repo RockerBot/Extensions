a55b1c5df7f9303183ec4d9f0bab1796c82e11d5
RockerBot <rockerbotmail@gmail.com>
2023-06-14 15:36:50 +0530

    tidBits restore+cs2

---
+++ tidBits/background.js
@@ -0,0 +1,17 @@
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo)=>{
+    if (changeInfo.url === undefined)return;
+    browser.storage.local.get(['tidBits_tabs'], (res) => {
+        var obj = (res.tidBits_tabs !== undefined)?res.tidBits_tabs : {};
+        obj[tabId] = changeInfo.url;
+        browser.storage.local.set({ tidBits_tabs: obj})
+    });
+});
+browser.tabs.onRemoved.addListener((tabId, removeInfo)=>{
+    if(removeInfo.isWindowClosing)return
+    browser.storage.local.get(['tidBits_tabs'], (res) => {
+        if(!res.tidBits_tabs)return;
+        var obj = res.tidBits_tabs;
+        delete obj[tabId];
+        browser.storage.local.set({ tidBits_tabs: obj})
+    });
+});
--- tidBits/popup/tidBits.js
+++ tidBits/popup/tidBits.js
@@ -1,22 +1,51 @@
-const IDs = ['shorts', 'pesu']
+const IDs_toglable = ['shorts', 'pesu']
 function id2key(id){ return "tidBits_" + id + "_enabled"; }
-function handleStartup(result, id){
-    document.getElementById(id).className = (!!result[id2key(id)])?"":"disabled";
+function handleStartup(res, id){
+    document.getElementById(id).className += (!!res[id2key(id)])?"":"disabled";
+    console.log(res,document.getElementById(id))
 }
-for(let id of IDs) {
+
+for(let id of IDs_toglable) {
     key = id2key(id);
     browser.storage.local.get( [key], res => handleStartup(res, id) );
 }
 
-document.addEventListener("click", (e) => {
-    const targ = e.target
-    const ID = targ.id;
-    if(ID !== 'shorts' && ID !== 'pesu' )return;
+document.addEventListener("click", e => {
+    const classNames = e.target.className
+    if(!classNames.includes("Clickable") )return;
+    if(classNames.includes("Toglable") )handleToglable(e.target);
+    else handleOtherClickable(e.target);    
+});
+
+function handleToglable(targ){
     targ.classList.toggle("disabled")
-    key = id2key(ID);
+    key = id2key(targ.id);
     browser.storage.local.get([key], (result) => {
         let obj = {};
         obj[key] = !result[key];
         browser.storage.local.set(obj);
     });
-});
+}
+function handleOtherClickable(targ){
+    if( targ.id === 'restore'){
+        browser.storage.local.get(['tidBits_tabs'], (result) => {
+            if(!result.tidBits_tabs)return;
+            browser.storage.local.set({ tidBits_tabs: undefined})
+            for (tabId in result.tidBits_tabs){
+                tabUrl = result.tidBits_tabs[tabId]
+                // console.log("tabs  -URL", tabUrl);
+                browser.tabs.create({ url:tabUrl }).catch(e=>console.log('ErR',e))
+            }
+        });
+    }
+    else if(targ.id === 'cs2'){
+        browser.tabs.query({ url:"https://www.youtube.com/@CaptainSparklez2/videos" })
+        .then(tabList=>{
+            if (tabList.length >0) browser.tabs.update( tabList[0].id,{active: true})
+            else browser.tabs.create({
+                active: true,
+                url:"https://www.youtube.com/@CaptainSparklez2/videos",
+            });
+        });
+    }
+}
